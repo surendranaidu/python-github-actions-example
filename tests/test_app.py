b0251ef09423ec3c01c5b0656770f6d2c9c568ba
surendra <31211689+surendranaidu@users.noreply.github.com>
2021-03-25 16:57:11 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
 from app import index
+from flask import Flask, render_templat
+
+
 
 def test_index():
-    assert index() == "surendra python dev"
+    #assert index() == "surendra python dev"
+    assert index() == render_template('python.html') 
