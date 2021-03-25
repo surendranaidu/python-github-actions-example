3dc5e818baec0282ab5d40a3f3643136b4c1cba0
surendra <31211689+surendranaidu@users.noreply.github.com>
2021-03-25 17:05:18 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('python.html'), "surendra python dev"
+    return render_template('python.html')
 
 if __name__ == "__main__":
     app.run()
